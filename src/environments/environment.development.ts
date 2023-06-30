05f5168c2470c628da817769796f28fe96452596
UshaAlcor <134499074+UshaAlcor@users.noreply.github.com>
2023-06-30 14:15:38 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiBase: "https://pbqm2qao2m.execute-api.ap-northeast-1.amazonaws.com/development/",
+    apiBase: " https://xeivk8ieq3.execute-api.ap-northeast-1.amazonaws.com/dev",
     env: 'dev'
 };
